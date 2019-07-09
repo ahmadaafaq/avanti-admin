329af19a087a8ffda8af14310728bc4f8eb929b5
Aafaq Ahmad <aafaq.ahmad@rsystems.com>
2019-07-09 14:11:26 +0530

    Updates

---
+++ precache-manifest.3d1ac8f45f512503538525ebbf7d4e41.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "e33c30175600d2d6ba37",
+    "url": "/avanti-admin/static/js/7.e33c3017.chunk.js"
+  },
+  {
+    "revision": "2543e14e08fcc3b97c47",
+    "url": "/avanti-admin/static/css/main.350c39f8.chunk.css"
+  },
+  {
+    "revision": "d9a52e139437ac98c3d6",
+    "url": "/avanti-admin/static/js/1.d9a52e13.chunk.js"
+  },
+  {
+    "revision": "a05e619546cd8dfb7e82",
+    "url": "/avanti-admin/static/js/2.a05e6195.chunk.js"
+  },
+  {
+    "revision": "621a9b00004313915c41",
+    "url": "/avanti-admin/static/js/3.621a9b00.chunk.js"
+  },
+  {
+    "revision": "bd9dd93443937161bbc2",
+    "url": "/avanti-admin/static/js/4.bd9dd934.chunk.js"
+  },
+  {
+    "revision": "b02bea24d7892b850463",
+    "url": "/avanti-admin/static/js/5.b02bea24.chunk.js"
+  },
+  {
+    "revision": "fc88216efc8d6ca57c71",
+    "url": "/avanti-admin/static/js/6.fc88216e.chunk.js"
+  },
+  {
+    "revision": "2543e14e08fcc3b97c47",
+    "url": "/avanti-admin/static/js/main.2543e14e.chunk.js"
+  },
+  {
+    "revision": "2456155c4c01b5389891",
+    "url": "/avanti-admin/static/js/8.2456155c.chunk.js"
+  },
+  {
+    "revision": "8cbad1b92887908c49a6",
+    "url": "/avanti-admin/static/js/9.8cbad1b9.chunk.js"
+  },
+  {
+    "revision": "07ead02adfe00b2eeb85",
+    "url": "/avanti-admin/static/js/10.07ead02a.chunk.js"
+  },
+  {
+    "revision": "1d0f8d10a2091b359645",
+    "url": "/avanti-admin/static/js/11.1d0f8d10.chunk.js"
+  },
+  {
+    "revision": "01515133a2eb6658c2f3",
+    "url": "/avanti-admin/static/js/12.01515133.chunk.js"
+  },
+  {
+    "revision": "cd1db9effed0c9a5518d",
+    "url": "/avanti-admin/static/js/runtime~main.cd1db9ef.js"
+  },
+  {
+    "revision": "f1fa8cf3b2ea3d2cede76100136b3386",
+    "url": "/avanti-admin/static/media/app_icon.f1fa8cf3.png"
+  },
+  {
+    "revision": "bcc2ac25ad3b64ddc278b471f7bf5b7a",
+    "url": "/avanti-admin/index.html"
+  }
+];
