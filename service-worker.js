319616055fb101ac4f2e524b22fbeda38a7e0908
Aafaq Ahmad <aafaq.ahmad@rsystems.com>
2019-07-09 14:33:39 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/avanti-admin/precache-manifest.3d1ac8f45f512503538525ebbf7d4e41.js"
+  "/avanti-admin/precache-manifest.195a4b5da3c25350b758f03797715661.js"
 );
 
 workbox.clientsClaim();
